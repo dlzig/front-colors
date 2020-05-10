d6be514ce422210ddb7173382b285e066a0a7692
arisa.satomura <dl.zig7@gmail.com>
2020-05-10 14:07:11 +0000

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   get 'toppages/index'
   root to: 'toppages#index'
+  
+  get 'signup', to: 'users#new'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   resources :artworks
-  resources :color, only: [:create, :destroy]
+  resources :colors, only: [:create, :destroy]
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_10_022028) do
+ActiveRecord::Schema.define(version: 2020_05_10_112111) do
 
   create_table "artworks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 2020_05_10_022028) do
 
   create_table "colors", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "hue"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.string "profile"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
